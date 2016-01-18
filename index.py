dda3f5992847baf6922b8c94b44da19fe9e18ce7
411030589@qq.com <411030589@qq.com>
2016-01-18 10:57:49 +0800

    update

--- index.py
+++ index.py
@@ -5,18 +5,50 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 import pdb
+import shutil
+import os
 
-host_path = 'C:\Windows\System32\drivers\etc\hosts'
+print """
+        ********************
+        * Power by Kenji   *
+        *        2016.1.18 *
+        ********************
+      """
+print "Model list"
+print '1:change hosts for google/facebook/twitter...'
+print '2:resore origin hosts'
+choice_model = raw_input("select model\r\n")
 
-content_obj = urllib2.urlopen('http://zeus.softweek.net/item-slt-1.html')
-content = content_obj.read()
-re_a_href = '^http://file.*?.txt$'
-find_result = re.findall(r'http://.*?.txt', content)
-google_host_content_obj = urllib2.urlopen(find_result[0])
-google_host_content = google_host_content_obj.read()
-host_content = open(host_path, 'w+')
-host_content.write(google_host_content)
 
+def change_host():
+    host_path = 'C:\Windows\System32\drivers\etc\hosts'
+    content_obj = urllib2.urlopen('http://zeus.softweek.net/item-slt-1.html')
+    content = content_obj.read()
+    if os.path.exists(host_path + "1.backup"):
+        shutil.copy(host_path + "1.backup", host_path)
+    else:
+        shutil.copy(host_path, host_path + "1.backup")
+    re_a_href = '^http://file.*?.txt$'
+    find_result = re.findall(r'http://.*?.txt', content)
+    google_host_content_obj = urllib2.urlopen(find_result[0])
+    google_host_content = google_host_content_obj.read()
+    host_content = open(host_path, 'a+')
+    host_content.write(google_host_content)
+
+def restore_hosts():
+    host_path = 'C:\Windows\System32\drivers\etc\hosts'
+    if os.path.exists(host_path + "1.backup"):
+        shutil.copy(host_path + "1.backup", host_path)
+    else:
+        print "Error !!! File not found !!!\r\n" + host_path + "1.backup"
+        exit()
+    pass
+
+
+if __name__ == "__main__":
+    model_list = {"1": change_host,"2": restore_hosts}
+    model_list[choice_model]()
+    raw_input("Success !!! \r\npress any key exit")
 
 
 
